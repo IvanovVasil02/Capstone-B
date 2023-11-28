e3228a5f331818c8bf0d7720e81584e25b7a3fae
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-11-28 15:31:33 +0100

    first commit

---
+++ src/main/java/com/example/CapstoneB/CapstoneBApplication.java
@@ -0,0 +1,13 @@
+package com.example.CapstoneB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CapstoneB/CapstoneBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CapstoneB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
