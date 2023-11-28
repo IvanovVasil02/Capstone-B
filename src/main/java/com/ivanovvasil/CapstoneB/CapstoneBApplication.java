feb9ba594925685f5ab11a757675c0d74a192464
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-11-28 15:31:50 +0100

    first commit

--- src/main/java/com/ivanovvasil/CapstoneB/CapstoneBApplication.java
+++ src/main/java/com/ivanovvasil/CapstoneB/CapstoneBApplication.java
@@ -1,4 +1,4 @@
-package com.example.CapstoneB;
+package com.ivanovvasil.CapstoneB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/ivanovvasil/CapstoneB/Farmaco/Farmaco.java
@@ -0,0 +1,4 @@
+package com.ivanovvasil.CapstoneB.Farmaco;
+
+public class Farmaco {
+}
