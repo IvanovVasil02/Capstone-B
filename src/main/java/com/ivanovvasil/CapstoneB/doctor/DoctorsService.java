c0a54666f6089986e52c4256e7f6f51f7fdd14a8
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-12-03 10:09:31 +0100

    adding of method to save doctor for the runner and method to change doctors data

---
+++ src/main/java/com/ivanovvasil/CapstoneB/doctor/DoctorsService.java
@@ -0,0 +1,31 @@
+package com.ivanovvasil.CapstoneB.doctor;
+
+import com.ivanovvasil.CapstoneB.exceptions.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class DoctorsService {
+  @Autowired
+  DoctorsRepo dr;
+
+  public Doctor save(Doctor doctor) {
+    dr.save(doctor);
+    return null;
+  }
+
+  public Doctor findById(UUID id) {
+    return dr.findById(id).orElseThrow(() -> new NotFoundException(id));
+  }
+
+  public Doctor findByIdAndUpdate(UUID id, DoctorUpdateDTO body) {
+    Doctor doctor = this.findById(id);
+    doctor.setEmail(body.email());
+    doctor.setEmergencyContact(body.emergencyContact());
+    doctor.setProvince(body.province());
+    doctor.setPhoneNumber(body.phoneNumber());
+    return dr.save(doctor);
+  }
+}
