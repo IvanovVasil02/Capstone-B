856b58dcdc40cf882be5bc77ab6603950b563dca
IvanovVasil02 <ivanov.vasil235@gmail.com>
2024-01-08 01:47:03 +0100

    Introduced MedicineDTO and renamed Patient DTO

--- src/main/java/com/ivanovvasil/CapstoneB/prescription/payloads/MedicinePrescriptionDTO.java
+++ src/main/java/com/ivanovvasil/CapstoneB/prescription/payloads/MedicinePrescriptionDTO.java
@@ -1,9 +1,11 @@
 package com.ivanovvasil.CapstoneB.prescription.payloads;
 
 import com.ivanovvasil.CapstoneB.prescription.PrescriptionDetails;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.Set;
 
-public record PatientPrescriptionDTO(
+public record MedicinePrescriptionDTO(
+        @NotEmpty(message = "Empty prescription!")
         Set<PrescriptionDetails> prescription) {
 }
